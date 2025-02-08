c06ca879e5ce7250a7817fa946deddd501df3ac5
Kai Nguyen <>
2025-02-08 14:32:53 +0700

    Init project to git

---
+++ src/main/java/com/kai/Spring_docker_init_template/SpringDockerInitTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.kai.Spring_docker_init_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerInitTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerInitTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kai/Spring_docker_init_template/SpringDockerInitTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kai.Spring_docker_init_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerInitTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
